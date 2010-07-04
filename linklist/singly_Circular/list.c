e936e240f5acd7487c3cfc960db16ccdcdfb2d8a
Atul R. Raut <atulraut17@gmail.com>
2010-07-04 20:22:43 +0530

    function defination for init and create function.

--- linklist/singly_Circular/list.c
+++ linklist/singly_Circular/list.c
@@ -1,19 +1,52 @@
+/*
+* Aim : Singly Circular Link List
+* Author : Atul R. Raut
+* Date   : Sunday, July 04 2010, 07:05 PM
+* File   : list.c function defination.
+***/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
 
-void init(LIST *p1) {
-
+void init(NODE *p1) {
+	p1->data = 0;
+	p1->next = NULL;
+	p1->prev = NULL;
 }
 
-void addfirst(LIST *p1, int data) {}
+NODE* create (int data) {
+	NODE *nn = NULL;
+	nn = (NODE *) malloc (sizeof(NODE));
+	if (nn == NULL)
+		return;
+	else {
+		nn->data = data;
+		nn->next = NULL;
+		nn->prev = NULL;
+	}
+	return nn;
+}
+
+void addfirst(LIST *p1, int data) {
+	NODE *nn = NULL;
+	if (p1->head == NULL) {
+		p1 = nn;
+		p1->next = p1->prev;	// Circular ll starts here.
+	}
+	else {
+		nn = create(data);
+		nn->prev = p1->head->prev;
+		nn->next = p1->head;
+	}
+}
 
 void insert(LIST *, int data, int pos) {}
 
 void addlast(LIST *p1, int data) {}
 void delfirst(LIST *p1) {}
 void deletenode(LIST *p1, int data) {}
-  void dellast(LIST *p1) {}
+void dellast(LIST *p1) {}
 void display(LIST *p1) {}
 void reverse(LIST *p1) {}
 void printrev(LIST *p1, LIST *p2) {}
--- linklist/singly_Circular/list.h
+++ linklist/singly_Circular/list.h
@@ -1,21 +1,33 @@
+/*
+* Aim : Create Singly Circular Link List Using head tail mechanism.
+* Author : Atul Ramesh Raut
+* Date   : Sunday, July 04 2010, 02:44 PM
+* File   : list.h, function prototype.
+***/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
 
-void init(LIST *p1) {
+typedef struct node_t {
+	int data;	
+	struct node_t *next;
+	struct node_t *prev;
+} NODE;
 
-}
+typedef struct list_t {
+	NODE *head;
+} LIST;
 
-void addfirst(LIST *p1, int data) {}
-
-void insert(LIST *, int data, int pos) {}
-
-void addlast(LIST *p1, int data) {}
-void delfirst(LIST *p1) {}
-void deletenode(LIST *p1, int data) {}
-  void dellast(LIST *p1) {}
-void display(LIST *p1) {}
-void reverse(LIST *p1) {}
-void printrev(LIST *p1, LIST *p2) {}
-void freelist(LIST *p1) {}
+void init(NODE *p1);
+NODE* create (int data);
+void addfirst(LIST *p1, int data); 
+void insert(LIST *, int data, int pos); 
+void addlast(LIST *p1, int data); 
+void delfirst(LIST *p1);
+void deletenode(LIST *p1, int data); 
+void dellast(LIST *p1); 
+void display(LIST *p1); 
+void reverse(LIST *p1);
+void printrev(LIST *p1, LIST *p2); 
+void freelist(LIST *p1); 
 
